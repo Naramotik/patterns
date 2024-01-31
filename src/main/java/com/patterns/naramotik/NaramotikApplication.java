c1bb1aadf1febac81f29457fe70e8b63678a663a
Naramotik <antwoodlive@yandex.ru>
2024-01-31 17:31:21 +0300

    first commit

---
+++ src/main/java/com/patterns/naramotik/NaramotikApplication.java
@@ -0,0 +1,13 @@
+package com.patterns.naramotik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaramotikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaramotikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patterns/naramotik/NaramotikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patterns.naramotik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaramotikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
